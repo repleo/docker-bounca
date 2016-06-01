40d65d99ed37314b7c9e4d3b472872d0c914fff3
Jeroen Arnoldus <jeroen@repleo.nl>
2016-06-01 07:34:12 +0200

    Added version tagging to docker push script

--- push-bounca-to-docker-hub.sh
+++ push-bounca-to-docker-hub.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
-docker build -t "repleo:bounca" .
-docker push repleo/bounca
+if [ -z ${1} ]; 
+then 
+ echo "Please provide the version tag as first argument. For example: ./push-bounca-to-docker-hub.sh v0.1.0";
+ exit 1
+fi 
+
+docker build -t "repleo/bounca:$1" -t "repleo/bounca:latest" .
+docker push repleo/bounca:$1
+docker push repleo/bounca:latest
+
